52af7beb16fc05c4ac62f327e84844bdb5ae180c
auductuan <147390394+auductuan@users.noreply.github.com>
2024-01-11 10:01:05 +0700

    Add files via upload

---
+++ bai moi/index.js
@@ -0,0 +1,22 @@
+// console.log("Hello world");s
+// console.log("Con chó này");
+// console.log("Trường hải phòng")
+// console.log("15-16 rách");
+// console.log("Hoàng tử Long Biên ");
+// console.log("Quang wjbu rách che mắt");
+
+
+// var fullName ="Bảo Bốc C";
+// let job = "CV";
+// const age = 40;
+
+// console.log(fullName);
+// console.log(job);
+// console.log(age);
+
+// prompt("Mời mày nhập tên vào đây");
+
+let a = prompt("Nhập số a")
+let b = prompt("Nhập số b")
+let tong = Number(a) + Number(b);
+console.log("tổng 2 số a, b là " + tong)
